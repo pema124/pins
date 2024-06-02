7c8eb1882837aba51649a5c13e29412c6f5ea9f8
pema124 <peter.mathes@gmx.de>
2024-06-02 20:49:40 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,23 @@
-input.onPinTouchEvent(TouchPin.P0, input.buttonEventValue(ButtonEvent.Click), function () {
-    let GangMax = 0
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventValue(ButtonEvent.Down), function () {
     if (Gang >= GangMax) {
         Gang = 0
     } else {
         Gang = Gang + 1
     }
-    basic.showNumber(Gang)
+    if (Gang == 0) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # # . .
+            # . # . .
+            # . . # .
+            `)
+    } else {
+        basic.showNumber(Gang)
+    }
 })
+let GangMax = 0
 let Gang = 0
-Gang = 0
+Gang = 1
+GangMax = 3
+basic.showNumber(Gang)
